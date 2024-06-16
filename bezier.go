b7bd2b1743a1f87b9748c13333e24ca05459840a
peugeot <thomaspeugeot@yahoo.fr>
2024-06-16 12:01:57 +0200

    WIP

--- bezier.go
+++ bezier.go
@@ -1,55 +1,56 @@
 package main
 
-import(
-    "math"
+import (
 	"gonum.org/v1/plot"
 	"gonum.org/v1/plot/plotter"
 	"gonum.org/v1/plot/plotutil"
 	"gonum.org/v1/plot/vg"
+	"math"
 )
 
-var x = []float64{0, 3.5, 25, 25, -5,-5, 15, -0.5, 19.5, 7, 1.5}
+var x = []float64{40, 3.5, 25, 25, -5, -5, 15, -0.5, 19.5, 7, 1.5}
 var y = []float64{0, 36, 25, 1.5, 3, 33, 11, 35, 15.5, 0, 10.5}
-var waga = []float64{1,6,4,2,3,2,2,1,5,4,1}
 
-func Newton(n int, k int) float64{
-   var rez float64 = 1
-   	for i := 1; i < k; i++ {
-		rez= rez* float64(n-i+1.0) / float64(i)
+func Newton(n int, k int) float64 {
+	var rez float64 = 1
+	for i := 1; i < k; i++ {
+		rez = rez * float64(n-i+1.0) / float64(i)
 	}
-   return rez
+	return rez
 }
 
-func Bernstein(n int, i int, t float64) float64{
-    var rez = math.Pow(float64(t),float64(i)) * math.Pow(float64(1-t),float64(n-i))
-    return Newton(n,i) * rez
+func Bernstein(n int, i int, t float64) float64 {
+	var rez = math.Pow(float64(t), float64(i)) * math.Pow(float64(1-t), float64(n-i))
+	return Newton(n, i) * rez
 }
+
+var nbPlots = 200
 
 func Points() plotter.XYs {
-   var n int = 10
-   pts := make(plotter.XYs, 101)
+	var n int = 10
+	pts := make(plotter.XYs, nbPlots+1)
 
-   for z:=0;z<=100;z++ {
-    var current_x,current_y,t float64  = 0.0, 0.0, float64(z)/100.0
-    var temp float64
-    var denominator float64 = 0.0
-      
-   for i:=0;i<=n;i++ {
-         temp = waga[i] * x[i] * Bernstein(n,i,t)
-        current_x += temp  
-         temp = waga[i] * y[i] * Bernstein(n,i,t)
-        current_y += temp   
-         temp = waga[i] * Bernstein(n,i,t)
-        denominator += temp     
-        }
+	for z := 0; z <= nbPlots; z++ {
+		var current_x, current_y, t float64 = 0.0, 0.0, float64(z) / float64(nbPlots)
+		var temp float64
+		var denominator float64 = 0.0
 
-     pts[z].X = current_x / denominator
-	 pts[z].Y = current_y / denominator
-   }
-    
-   return pts
+		for i := 0; i <= n; i++ {
+			temp = x[i] * Bernstein(n, i, t)
+			current_x += temp
+			temp = y[i] * Bernstein(n, i, t)
+			current_y += temp
+			temp = Bernstein(n, i, t)
+			denominator += temp
+		}
+
+		pts[z].X = current_x / denominator
+		pts[z].Y = current_y / denominator
+	}
+
+	return pts
 }
-func main(){
+func main() {
 
 	p := plot.New()
 
@@ -66,11 +67,3 @@ func main(){
 		panic(err)
 	}
 }
-
-
-
-
-
-
-
-
